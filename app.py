59369507a74e8e174087d45103f9c7692ac72cc0
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-30 20:56:30 -0500

    Use session to store responses list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -6,13 +6,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "abc123"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
-
-# ********************************************
-# GLOBAL VARIABLES
-# responses -> initialize responses; this is the variable that will store survey answers
-#     e.g. responses = ['Yes', 'No', 'Less than $10,000', 'Yes']
-responses = []
-# *********************************************
 
 
 @app.route('/')
@@ -32,16 +25,18 @@ def initialize_responses():
 def show_question(idx):
     """Shows question from the survey"""
 
-    if len(responses) >= len(survey.questions):
+    if len(session['responses']) >= len(survey.questions):
+        # User has answered all questions; thank them.
         return redirect('/thank-you')
 
-    if idx == len(responses):
+    if idx == len(session['responses']):
+        # 
         question = survey.questions[idx].question
         choices = survey.questions[idx].choices
         return render_template('question.html', survey=survey, question=question, choices=choices, idx=idx)
     else:
         flash("You are trying to access an invalid question.", "error")
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
 
 
 @app.route('/answer', methods=['POST'])
@@ -49,12 +44,14 @@ def add_answer():
     """Adds the answer to the pretend database and redirects the user"""
     answer = request.form['answer']
     # Add to pretend database
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
 
     # Redirect user to either the next question or
     # thank you page if there are no more questions.
-    if len(responses) < len(survey.questions):
-        return redirect(f'/questions/{len(responses)}')
+    if len(session["responses"]) < len(survey.questions):
+        return redirect(f"/questions/{len(session['responses'])}")
     else:
         return redirect('/thank-you')
 
